43648ad267582577c0e5fbfe79aff4c83c446d0c
Oadegbite <oadegbite@ryerson.ca>
2019-02-23 07:57:20 -0500

    added joker

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express')
 var app = express();
 var port = process.env.PORT || 3000;
+var faker = require('faker');
 
-app.get('/', function (req, res) {
-  res.send('hello world')
+app.get('/', function (req, res) {  
+  res.send(faker.name.findName())
 })
 
 app.listen(port, function() {
